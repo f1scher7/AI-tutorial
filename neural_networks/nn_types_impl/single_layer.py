41fea18a13766f6d4bafc5c8b6c10b9e4bcc01a2
f1scher7 <maksym.szylo@gmail.com>
2024-11-19 12:01:03 +0100

    Added fixes for single layer nn impl

--- neural_networks/nn_types_impl/single_layer.py
+++ neural_networks/nn_types_impl/single_layer.py
@@ -3,73 +3,72 @@ import numpy as np
 from time import perf_counter
 from utils.math.activation_funcs import activation_func
 from utils.math.activation_funcs import activation_derivative_func
-from utils.math.cost_funcs import *
+from utils.math.weights_initialization import *
+from utils.math.cost import *
 
-def train_single_layer_nn(X_f, y_f, hidden_neurons_f, epochs_f, learning_rate_f, plot_f):
+
+def train_single_layer_nn(x, y, epochs, learning_rate_f, hidden_neurons, in_to_hid_init_name, hid_to_out_init_name, hid_act_func_name, out_act_func_name, plot):
     from utils.visualisation import plot_mse, plot_decision_boundary, print_result_nn
 
     np.random.seed(42)
 
-    input_neurons = X_f.shape[1]
-    output_neurons = y_f.shape[1]
+    input_neurons = x.shape[1]
+    output_neurons = y.shape[1]
 
-    input_to_hidden_weights = np.random.uniform(low=-1, high=1, size=(input_neurons, hidden_neurons_f))
-    hidden_to_output_weights = np.random.uniform(low=-1, high=1, size=(hidden_neurons_f, output_neurons))
+    input_to_hidden_weights = weights_initialization_func((input_neurons, hidden_neurons), in_to_hid_init_name)
+    hidden_to_output_weights = weights_initialization_func((hidden_neurons, output_neurons), hid_to_out_init_name)
 
-    bias_hidden_weights = np.random.uniform(low=-1, high=1, size=(1, hidden_neurons_f))
-    bias_output_weights = np.random.uniform(low=-1, high=1, size=(1, output_neurons))
+    bias_hidden_weights = np.zeros((1, hidden_neurons))
+    bias_output_weights = np.zeros((1, output_neurons))
 
     final_nn_output = np.array([])
     mse_values = []
 
     start_time = perf_counter()
 
-    for epoch in range(epochs_f):
+    for epoch in range(epochs):
         # Forward propagation
-        hidden_pre_activation = np.dot(X_f, input_to_hidden_weights) + bias_hidden_weights # Potted sum for hidden layer
-        hidden_activated = activation_func(hidden_pre_activation, 'sigmoid') # Neurons activation for hidden layer
+        hidden_pre_activation = np.dot(x, input_to_hidden_weights) + bias_hidden_weights # Potted sum for hidden layer
+        hidden_activated = activation_func(hidden_pre_activation, hid_act_func_name) # Neurons activation for hidden layer
 
         output_pre_activation = np.dot(hidden_activated, hidden_to_output_weights) + bias_output_weights
-        output_activated = activation_func(output_pre_activation, 'sigmoid')
+        output_activated = activation_func(output_pre_activation, out_act_func_name)
 
         final_nn_output = output_activated
-        mse_values.append(cost_func(y_f, output_activated, 'mse'))
+        mse_values.append(cost_func(y, output_activated, 'mse'))
 
         # Back propagation
-        output_error = cost_derivative_func(y_f, output_activated, 'mse')
+        output_error = y - output_activated
         # Output gradient - CORRECTIONS for each component
         # Output error * sensitivity for each component = how each component contributed to output error
-        output_gradient = output_error * activation_derivative_func(output_activated, 'sigmoid')
+        output_gradient = output_error * activation_derivative_func(output_activated, out_act_func_name)
 
         # Replacing output_error to hidden layer
         hidden_error = np.dot(output_gradient, hidden_to_output_weights.T)
-        hidden_gradient = hidden_error * activation_derivative_func(hidden_activated, 'sigmoid')
+        hidden_gradient = hidden_error * activation_derivative_func(hidden_activated, hid_act_func_name)
 
         # Updating weights and biases
         hidden_to_output_weights += np.dot(hidden_activated.T, output_gradient) * learning_rate_f
-        input_to_hidden_weights += np.dot(X_f.T, hidden_gradient) * learning_rate_f
+        input_to_hidden_weights += np.dot(x.T, hidden_gradient) * learning_rate_f
 
         bias_output_weights += np.sum(output_gradient, axis=0, keepdims=True)
         bias_hidden_weights += np.sum(hidden_gradient, axis=0, keepdims=True)
 
-        if epoch % 100 == 0:
-            print(f"Epoch {epoch}, MSE: {mse_values[:-1]}")
+        if plot is not None:
+            plot_decision_boundary(x, input_to_hidden_weights, hidden_to_output_weights, bias_hidden_weights, bias_output_weights, hid_act_func_name, out_act_func_name, epoch, plot)
+            plot.pause(0.01)
 
-        if plot_f is not None:
-            plot_decision_boundary(X_f, input_to_hidden_weights, hidden_to_output_weights, bias_hidden_weights,bias_output_weights, epoch, plot_f)
-            plot_f.pause(0.01)
-
-    plot_f.show()
+    plot.show()
 
     end_time = perf_counter()
     training_time = end_time - start_time
 
-    print_result_nn(final_nn_output, epochs_f, training_time)
+    print_result_nn(final_nn_output, epochs, training_time)
     plot_mse(mse_values)
 
     return input_to_hidden_weights, hidden_to_output_weights, bias_hidden_weights, bias_output_weights
 
 
-def predict_single_layer_nn(X_f, input_to_hidden_weights_f, hidden_to_output_weights_f, bias_hidden_weights_f, bias_output_weights_f):
-    hidden_activated = activation_func(np.dot(X_f, input_to_hidden_weights_f) + bias_hidden_weights_f, 'sigmoid')
-    return activation_func(np.dot(hidden_activated, hidden_to_output_weights_f) + bias_output_weights_f, 'sigmoid')
+def predict_single_layer_nn(x, input_to_hidden_weights, hidden_to_output_weights, bias_hidden_weights, bias_output_weights, hid_act_func_name, out_act_func_name):
+    hidden_activated = activation_func(np.dot(x, input_to_hidden_weights) + bias_hidden_weights, hid_act_func_name)
+    return activation_func(np.dot(hidden_activated, hidden_to_output_weights) + bias_output_weights, out_act_func_name)
