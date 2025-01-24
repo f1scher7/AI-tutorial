e2edf5ea6727f5a7edae9befa2c163b58d22b5d4
Maks Szyło <maksym.szylo@gmail.com>
2025-01-25 00:21:12 +0100

    Renamed cost.py to loss.py and added some fixes

--- utils/math/loss.py
+++ utils/math/loss.py
@@ -4,6 +4,7 @@ import numpy as np
 def cost_func(y_true, y_pred, cost_func_name):
     return cost_funcs[cost_func_name](y_true, y_pred)
 
+
 def cost_derivative_func(y_true, y_pred, cost_func_name):
     return cost_derivative_funcs[cost_func_name](y_true, y_pred)
 
@@ -13,5 +14,5 @@ cost_funcs = {
 }
 
 cost_derivative_funcs = {
-    'mse': lambda y_true, y_pred: 2 * (y_pred - y_true) / y_true.shape[0]
+    'mse': lambda y_true, y_pred: 2 * (y_true - y_pred) / y_true.shape[0]
 }
