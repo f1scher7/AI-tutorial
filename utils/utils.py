cf0cabd875156ddc5787073664b7fdae988b7aa7
Maks Szyło <maksym.szylo@gmail.com>
2025-01-25 22:20:56 +0100

    Added utis.py

---
+++ utils/utils.py
@@ -0,0 +1,18 @@
+import numpy as np
+from datetime import datetime
+from env_loader import SAVED_MODELS_PATH
+
+
+def save_nn_model(file_name, model_info):
+    timestamp =datetime.now().strftime("%Y.%m.%d-%H:%M:%S")
+
+    file = f'{SAVED_MODELS_PATH}{file_name}_{timestamp}'
+    np.save(file, model_info)
+
+    print(f"{file_name} was saved to {file}")
+
+
+def load_saved_nn_model(file_name):
+    nn_model_info = np.load(f'{SAVED_MODELS_PATH}{file_name}', allow_pickle=True).item()
+
+    return nn_model_info
