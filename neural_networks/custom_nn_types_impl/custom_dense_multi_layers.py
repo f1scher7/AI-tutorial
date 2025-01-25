Added train and save funcs and added fixes for backpropagation

neural_networks/custom_nn_types_impl/custom_dense_multi_layers.py
@@ -4,19 +4,21 @@ from utils.math.activation_funcs import activation_func, activation_derivative_f
 from utils.math.weights_initialization import weights_initialization_func
 from utils.math.biases_initialization import biases_initialization_func
 from utils.math.loss import training_loss_func, training_loss_derivative_func
+from utils.visualisation import print_training_logs_nn, plot_training_losses
+from utils.utils import save_nn_model
 
 
 class CustomDenseMultiLayerNN:
 
     def __init__(self, problem_name, input_data, target, hidden_neurons_list, activation_funcs_list,
-                 weights_initialization_types, training_loss_function_name, epochs, learning_rate, momentum=0.9, reg_type=None, reg_lambda=0.1):
+                 weights_initialization_types_list, training_loss_function_name, epochs, learning_rate, momentum=0.9, reg_type=None, reg_lambda=0.1):
 
         self.problem_name = problem_name
         self.input_data = input_data
         self.target = target
         self.hidden_neurons_list = hidden_neurons_list
         self.activation_funcs_list = activation_funcs_list
-        self.weights_initialization_types = weights_initialization_types
+        self.weights_initialization_types_list = weights_initialization_types_list
         self.training_loss_function_name = training_loss_function_name
 
         self.epochs = epochs
@@ -36,20 +38,19 @@ class CustomDenseMultiLayerNN:
         self.training_losses = []
 
         # adding weights for input -> hidden1 layer
-        self.weights.append(weights_initialization_func((self.sequence_len, hidden_neurons_list[0]), self.weights_initialization_types[0]))
+        self.weights.append(weights_initialization_func((self.sequence_len, hidden_neurons_list[0]), self.weights_initialization_types_list[0]))
         self.biases.append(biases_initialization_func(self.hidden_neurons_list[0], self.activation_funcs_list[0]))
 
         if self.num_hidden_layers > 1:
             for layer_idx in range(1, self.num_hidden_layers):
-                self.weights.append(weights_initialization_func((self.hidden_neurons_list[layer_idx - 1], self.hidden_neurons_list[layer_idx]), weights_initialization_types[layer_idx]))
+                self.weights.append(weights_initialization_func((self.hidden_neurons_list[layer_idx - 1], self.hidden_neurons_list[layer_idx]), weights_initialization_types_list[layer_idx]))
                 self.biases.append(np.zeros(self.hidden_neurons_list[layer_idx]))
 
         # adding weights and biases for hidden_last -> output layer
-        self.weights.append(weights_initialization_func((hidden_neurons_list[-1], self.target.shape[1]), weights_initialization_types[-1]))
+        self.weights.append(weights_initialization_func((hidden_neurons_list[-1], self.target.shape[1]), weights_initialization_types_list[-1]))
         self.biases.append(biases_initialization_func(self.target.shape[1], self.activation_funcs_list[-1]))
 
         self.velocity_weights = [np.zeros_like(w) for w in self.weights]
-        self.velocity_biases = [np.zeros_like(b) for b in self.biases]
 
 
     def forward_propagation(self):
@@ -81,42 +82,101 @@ class CustomDenseMultiLayerNN:
         d_weights = [np.zeros_like(w) for w in self.weights]
         d_biases = [np.zeros_like(b) for b in self.biases]
 
+        batch_loss = 0
+
         for i in range(self.batch_size):
-            y_true = self.target[i]
-            y_pred = activated_outputs[i]
+            batch_loss += training_loss_func(y_true=self.target[i], y_pred=activated_outputs[i], training_loss_func_name=self.training_loss_function_name)
+            error = training_loss_derivative_func(y_true=self.target[i], y_pred=activated_outputs[i], training_loss_func_name=self.training_loss_function_name)
 
-            self.training_losses.append(training_loss_func(y_true=y_true, y_pred=y_pred, training_loss_func_name=self.training_loss_function_name))
-            error = training_loss_derivative_func(y_true=y_true, y_pred=y_pred, training_loss_func_name=self.training_loss_function_name)
+            delta = error * activation_derivative_func(activated_outputs[i], self.activation_funcs_list[-1])
 
-            delta = error * activation_derivative_func(y_pred, self.activation_funcs_list[-1])
+            d_weights[-1] += np.dot(activated_hidden_layers[i][-1].T, delta)
+            d_biases[-1] += np.sum(delta, axis=0)
 
-            d_weights[-1] += np.outer(activated_hidden_layers[-1], delta)
-            d_biases[-1] += delta
+            prev_delta = delta
 
             for layer_idx in range(self.num_hidden_layers - 1, -1, -1):
-                error = np.dot(delta, self.weights[layer_idx + 1].T)
-                delta = error * activation_derivative_func(activated_hidden_layers[layer_idx], self.activation_funcs_list[layer_idx])
+                if layer_idx == self.num_hidden_layers - 1:
+                    error = np.dot(prev_delta, self.weights[-1].T)
+                else:
+                    error = np.dot(prev_delta, self.weights[layer_idx + 1].T)
 
-                prev_activation_layer = self.input_data[i] if layer_idx == 0 else activated_hidden_layers[layer_idx - 1]
+                delta = error * activation_derivative_func(activated_hidden_layers[i][layer_idx], self.activation_funcs_list[layer_idx])
 
-                d_weights[layer_idx] += np.outer(prev_activation_layer, delta)
-                d_biases[layer_idx] += delta
+                prev_layer = self.input_data[i] if layer_idx == 0 else activated_hidden_layers[i][layer_idx - 1]
 
+                d_weights[layer_idx] += np.dot(prev_layer.T, delta)
+                d_biases[layer_idx] += np.sum(delta, axis=0)
+
+                prev_delta = delta
+
+        self.training_losses.append(batch_loss / self.batch_size)
+
+        # Average gradients
         d_weights = [dw / self.batch_size for dw in d_weights]
         d_biases = [db / self.batch_size for db in d_biases]
 
-        for layer_idx in range(len(self.weights)):
+        for layer_idx in range(self.num_hidden_layers):
             self.velocity_weights[layer_idx] = (
                 self.momentum * self.velocity_weights[layer_idx] - self.learning_rate * d_weights[layer_idx]
             )
 
-            self.velocity_biases[layer_idx] = (
-                self.momentum * self.velocity_biases[layer_idx] - self.learning_rate * d_biases[layer_idx]
-            )
-
-            # add additional "speed" for weights
+            # Add additional "speed" for weights
             self.weights[layer_idx] += self.velocity_weights[layer_idx]
-            self.biases[layer_idx] += self.velocity_biases[layer_idx]
+
+            self.biases[layer_idx] -= self.learning_rate * d_biases[layer_idx]
+
+        return self.weights, self.biases
+
+
+    def save(self):
+        model_info = {
+            "weights": self.weights,
+            "biases": self.biases,
+
+            "hidden_neurons_list": self.hidden_neurons_list,
+            "activation_funcs_list": self.activation_funcs_list,
+            "weights_initialization_types_list": self.weights_initialization_types_list,
+
+            "batch_size": self.batch_size,
+            "sequence_len": self.sequence_len,
+            "features_size": self.features_size,
+
+            "epochs": self.epochs,
+            "learning_rate": self.learning_rate,
+            "momentum": self.momentum,
+
+            "reg_type": self.reg_type,
+            "reg_lambda": self.reg_lambda,
+
+            "training_loss_function_name": self.training_loss_function_name,
+            "training_losses": self.training_losses,
+        }
+
+        file_name = f'CustomDenseMultiLayerNN_{self.problem_name}'
+
+        save_nn_model(file_name, model_info)
+
+
+    def train(self):
+        start_time = perf_counter()
+
+        for epoch in range(self.epochs):
+            activated_hidden_layers, activated_outputs = self.forward_propagation()
+            self.back_propagation(activated_hidden_layers, activated_outputs)
+
+        end_time = perf_counter()
+        training_time = end_time - start_time
+
+        print(f'Training time: {training_time:.2f} secs')
+
+        self.save()
+
+
+
+
+
+
 
 
 