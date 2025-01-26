0add838d75a9fe0f0c7ba35c6a37ee96bb636866
Maks Szyło <maksym.szylo@gmail.com>
2025-01-26 14:49:13 +0100

    Added fix for xavier

--- utils/math/weights_initialization.py
+++ utils/math/weights_initialization.py
@@ -12,6 +12,6 @@ weights_initialization_funcs = {
     'random': lambda shape: np.random.uniform(low=-1, high=1, size=shape),
     'normal': lambda shape: np.random.randn(*shape),
     'he': lambda shape: np.random.normal(loc=0, scale=np.sqrt(2 / shape[0]), size=shape),
-    'xavier': lambda shape: np.random.uniform(low=-np.sqrt(6 / (shape[0] + shape[1]), high=np.sqrt(6 / (shape[0] + shape[1]), size=shape)))
+    'xavier': lambda shape: np.random.uniform(low=-np.sqrt(6 / (shape[0] + shape[1])), high=np.sqrt(6 / (shape[0] + shape[1])), size=shape)
 }
 
