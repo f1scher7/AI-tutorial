6f6fb5896fba503844a0f3f4d041b5ebf89a47dc
Maks Szyło <maksym.szylo@gmail.com>
2025-01-27 20:38:34 +0100

    Added essential funcs for min-max-de/normalization

--- utils/math/data_normalization.py
+++ utils/math/data_normalization.py
@@ -4,33 +4,61 @@ import numpy as np
 # We're using Min-Max normalization:
 # when we have small spread in training data (age(18, 80));
 # when we want to have a range for training data;
-def min_max_normalization(data, feature_range=(0, 1)):
-    min_data = np.min(data)
-    max_data = np.max(data)
+def min_max_normalization_for_each_feature(data, norm_range=(0, 1)):
+    norm_data = np.zeros_like(data, dtype=float)
 
-    min_range, max_range = feature_range
+    min_max_for_each_features = []
+    min_feature_range, max_feature_range = norm_range
 
-    if np.any(max_data - min_data == 0):
-        print("WARNING! Min-Max normalization causes division by zero")
-        return data, None, None
+    for i in range(data.shape[2]):
+        min_feature = np.min(data[:, :, i])
+        max_feature = np.max(data[:, :, i])
+
+        if max_feature - min_feature == 0:
+            print("WARNING! Min-Max normalization causes division by zero")
+            return data, None
+
+        normalized_feature = (data[:, :, i] - min_feature) / (max_feature - min_feature)
+        scaled_normalized_feature = normalized_feature * (max_feature_range - min_feature_range) + min_feature_range
+
+        norm_data[:, :, i] = scaled_normalized_feature
+        min_max_for_each_features.append((min_feature, max_feature))
+
+    # Adding norm_range as a last element in the list
+    min_max_for_each_features.append(norm_range)
+
+    return norm_data, min_max_for_each_features
 
 
-# def min_max_normalization(x, normalization_range, min_val=None, max_val=None):
-#     if min_val is None:
-#         min_val = x.min(axis=0)
-#     if max_val is None:
-#         max_val = x.max(axis=0)
-#
-#     min_range, max_range = normalization_range
-#
-#     # For normalization range (0, 1)
-#     normalized_X = (x - min_val) / (max_val - min_val + 1e-10)
-#
-#     # For normalization range (a, b); for the (0, 1) normalization_X = scaled_X
-#     scaled_X = normalized_X * (max_range - min_range) + min_range
-#
-#     return scaled_X, min_val, max_val
+def min_max_normalization_for_each_feature_with_params(data, norm_params):
+    norm_data = np.zeros_like(data, dtype=float)
 
+    min_feature_range, max_feature_range = norm_params[-1]
+
+    for i in range(data.shape[2]):
+        min_feature, max_feature = norm_params[i]
+
+        normalized_feature = (data[:, :, i] - min_feature) / (max_feature - min_feature)
+        scaled_normalized_feature = normalized_feature * (max_feature_range - min_feature_range) + min_feature_range
+        norm_data[:, :, i] = scaled_normalized_feature
+
+    return norm_data
+
+
+def min_max_denormalization_for_each_feature(norm_data, norm_data_params):
+    denorm_data = np.zeros_like(norm_data, dtype=float)
+
+    min_feature_range, max_feature_range = norm_data_params[-1]
+
+    for i in range(norm_data.shape[2]):
+        min_feature, max_feature = norm_data_params[i]
+
+        unscaled_norm_feature = (norm_data[:, :, i] - min_feature_range) / (max_feature_range - min_feature_range)
+        denorm_feature = unscaled_norm_feature * (max_feature - min_feature) + min_feature
+
+        denorm_data[:, :, i] = denorm_feature
+
+    return denorm_data
 
 
 
