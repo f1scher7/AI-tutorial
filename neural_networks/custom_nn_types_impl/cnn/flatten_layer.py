eec518d2de3ae9035ee01189ec4d02a105a5cc19
Maks Szyło <maksym.szylo@gmail.com>
2025-02-04 20:25:53 +0100

    Added flatten layer

---
+++ neural_networks/custom_nn_types_impl/cnn/flatten_layer.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+class FlattenLayer:
+
+    def __init__(self):
+        self.input_shape = None
+
+
+    def forward_propagation(self, input_data):
+        self.input_shape = input_data.shape
+
+        batch_size = input_data.shape[0]
+
+        return input_data.reshape(batch_size, -1)
+
+
+    def back_propagation(self, output_data):
+        return output_data.reshape(self.input_shape)
