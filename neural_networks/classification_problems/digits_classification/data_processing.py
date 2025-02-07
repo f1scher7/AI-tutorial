ba07c09b7586e55d75d884e55b120e0aa5b58208
Maks Szyło <maksym.szylo@gmail.com>
2025-02-07 23:09:04 +0100

    Added labels_one_hot and some fixes

--- neural_networks/classification_problems/digits_classification/data_processing.py
+++ neural_networks/classification_problems/digits_classification/data_processing.py
@@ -11,6 +11,8 @@ def load_and_process_dataset():
     for label in range(10):
         folder_path = os.path.join(DIGITS_DATASET, str(label))
 
+        img_inc = 0
+
         for img_name in os.listdir(folder_path):
             img_path = os.path.join(folder_path, img_name)
 
@@ -25,16 +27,24 @@ def load_and_process_dataset():
                 binary_img = resized_img.point(lambda p: 255 if p > 64 else 0)
                 # binary_img.show()
 
-                data.append(np.array(binary_img).flatten())
+                data.append(np.array(binary_img))
                 labels.append(label)
+
+                img_inc += 1
             except Exception as e:
                 print(f'Error while loading {img_path}: {e}')
 
-        print(f"'{label}' images were processed")
+        print(f"'{label}' {img_inc} images were processed")
+
+    data_reshaped = np.array(data, dtype=np.uint8).reshape(-1, 28, 28, 1)
+    label_one_hot = np.eye(10, dtype=np.uint8)[labels]
+
+    print(data_reshaped.shape)
+    print(label_one_hot.shape)
 
     data_labels = {
-        "data": np.array(data),
-        "labels": np.array(labels)
+        "data": data_reshaped,
+        "labels_one_hot": label_one_hot
     }
 
     np.save(DIGITS_PROCESSED_DATASET_FILE, data_labels)
@@ -42,13 +52,14 @@ def load_and_process_dataset():
 
 def load_processed_dataset():
     data = np.load(DIGITS_PROCESSED_DATASET_FILE, allow_pickle=True).item()
-    return data["data"], data["labels"]
+    return data["data"], data["labels_one_hot"]
 
 
-data_test, labels_test = load_processed_dataset()
+# load_and_process_dataset()
 
-
-print(data_test[0])
-print(labels_test[0])
+# data_test, labels_test = load_processed_dataset()
+#
+# print(data_test[0])
+# print(labels_test[0])
 
 
