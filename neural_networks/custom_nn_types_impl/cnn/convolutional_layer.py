dc1ce2c85b416fdc92cce65898505b0794086f97
Maks Szyło <maksym.szylo@gmail.com>
2025-02-07 22:10:28 +0100

    Added some fixes

--- neural_networks/custom_nn_types_impl/cnn/convolutional_layer.py
+++ neural_networks/custom_nn_types_impl/cnn/convolutional_layer.py
@@ -6,7 +6,7 @@ from utils.math.activation_funcs import activation_func
 
 class ConvolutionLayer:
 
-    def __init__(self, input_shape, filters_num, filter_size, stride=1, padding=0, feature_map_activation_func='relu'):
+    def __init__(self, input_shape, filters_num, filter_size, filter_init_func_name='he', stride=1, padding=0, feature_map_activation_func='relu'):
         """
         input_shape: (width, height, channels)
         filter_size: (filter_width, filter_height)
@@ -14,6 +14,7 @@ class ConvolutionLayer:
         self.input_shape = input_shape
         self.filters_num = filters_num
         self.filter_size = filter_size
+        self.filter_init_func_name = filter_init_func_name
         self.stride = stride
         self.padding = padding
         self.feature_map_activation_func = feature_map_activation_func
@@ -25,8 +26,8 @@ class ConvolutionLayer:
         self.filter_width = self.filter_size[0]
         self.filter_height = self.filter_size[1]
 
-        self.filters = cnn_weights_initialization_func(shape=(self.filters_num, self.filter_height, self.filter_width, self.channels), func_name='he')
-        self.biases = biases_initialization_func(bias_len=self.filters_num, func_name='relu')
+        self.filters = cnn_weights_initialization_func(shape=(self.filters_num, self.filter_height, self.filter_width, self.channels), func_name=self.filter_init_func_name)
+        self.biases = biases_initialization_func(bias_len=self.filters_num, func_name=self.feature_map_activation_func)
 
         self.feature_map_width = (self.input_width - self.filter_width + 2 * self.padding) // self.stride + 1
         self.feature_map_height = (self.input_height - self.filter_height + 2 * self.padding) // self.stride + 1
