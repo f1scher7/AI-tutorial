2d5154a9f70d085a25a0c2893b1176532bcf478b
Maks Szyło <maksym.szylo@gmail.com>
2025-02-27 23:19:12 +0100

    Added some fixes

--- utils/math/loss.py
+++ utils/math/loss.py
@@ -8,10 +8,12 @@ epsilon = 1e-15
 def training_loss_func(y_true, y_pred, batch_size=None, training_loss_func_name='mse'):
     if training_loss_func_name == 'binary_cross_entropy' and batch_size is not None:
         y_pred = np.clip(y_pred, epsilon, 1. - epsilon)
-        return -1. / batch_size * np.sum(y_true * np.log(y_pred) + (1. - y_true) * np.log(1. - y_pred))
+        loss_per_feature = - np.sum(y_true * np.log(y_pred) + (1. - y_true) * np.log(1. - y_pred))
+        return np.mean(loss_per_feature)
     elif training_loss_func_name == 'categorical_cross_entropy' and batch_size is not None:
         y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
-        return -1. / batch_size * np.sum(y_true * np.log(y_pred))
+        loss_per_feature = - np.sum(y_true * np.log(y_pred))
+        return np.mean(loss_per_feature)
 
     return training_loss_funcs[training_loss_func_name](y_true, y_pred)
 
@@ -24,7 +26,7 @@ training_loss_funcs = {
 def training_loss_derivative_func(y_true, y_pred, batch_size=None, training_loss_func_name='mse'):
     if training_loss_func_name == 'binary_cross_entropy' and batch_size is not None:
         y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
-        return (y_pred - y_true) / (y_pred * (1 - y_true))
+        return (y_pred - y_true) / (y_pred * (1 - y_pred))
     elif training_loss_func_name == 'categorical_cross_entropy' and batch_size is not None:
         y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
         return - y_true / y_pred
